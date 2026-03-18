d11d46e5ddab7dd510a2f262891526cf81074b49
poojabadganchi <badganchipooja07@gmail.com>
2026-03-18 11:17:23 +0530

    Refactor toggleMenu function for clarity

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function toggleMenu(){
 document.getElementById("mainNav").classList.toggle("active");
 }
+
+function toggleMenu(){
+const nav = document.getElementById("mainNav");
+nav.classList.toggle("active");
+}
